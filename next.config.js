00ed018ac1fb96ada9aa0c72720d7316b447fb9a
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2026-02-23 10:07:43 -0300

    Fix GitHub Pages base path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/portfolio-website',
+  assetPrefix: '/portfolio-website',
   images: {
     unoptimized: true,
   },
