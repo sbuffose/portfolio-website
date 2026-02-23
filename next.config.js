b6769ca352f7492520a494a82d67e4819401da1f
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2026-02-23 10:04:29 -0300

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 }
 
